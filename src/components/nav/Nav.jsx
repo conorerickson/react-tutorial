a135c0e1f4852899fb72e905192cb9005ab7e96b
Conor Erickson <89137464+conorerickson@users.noreply.github.com>
2022-06-12 15:36:15 -0400

    Update Nav icons and Nav links

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { AiOutlineBook } from 'react-icons/ai'
+import { RiServiceLine } from 'react-icons/ri'
+import {AiOutlineMessage} from 'react-icons/ai'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><AiOutlineHome /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experience'><AiOutlineBook /></a>
+      <a href='#services'><RiServiceLine /></a>
+      <a href='#contact'><AiOutlineMessage /></a>
+    </nav>
   )
 }
 
